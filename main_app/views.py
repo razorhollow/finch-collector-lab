f5586876bfd3461623cdf6b7f1fcdcaf2e3f70dd
RazorHollow <rob@razorhollow.com>
2022-08-04 17:19:10 -0400

    add fish index functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Fish:
+  def __init__(self, name, description, category):
+    self.name = name
+    self.description = description
+    self.category = category
+
+fish = [
+  Fish('Largemouth Bass', 'my pointless description of what a largemouth bass is', 'fresh water')
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def fish_index(request):
+  return render(request, 'fish/index.html', { 'fish' : fish })
